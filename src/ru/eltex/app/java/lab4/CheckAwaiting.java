86888e74a85fc3b3d2d984fa40e4bf3fc84b0ea4
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-27 00:34:49 +0700

    Check-classes developed with overriding process() method

--- src/ru/eltex/app/java/lab4/CheckAwaiting.java
+++ src/ru/eltex/app/java/lab4/CheckAwaiting.java
@@ -1,13 +1,8 @@
 package ru.eltex.app.java.lab4;
 
-import ru.eltex.app.java.lab2.Order;
-import ru.eltex.app.java.lab2.OrderStatus;
-
 public class CheckAwaiting extends ACheck {
     @Override
-    public Order process(Order purchase) {
-        if(purchase.getStatus() == OrderStatus.AWAITING)
-            purchase.setStatus(OrderStatus.PROCESSED);
-        return purchase;
+    public int process() {
+        return ordersList.processElements();
     }
 }
