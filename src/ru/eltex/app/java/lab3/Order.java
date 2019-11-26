d91909456873db3fb558c4e77ea896991d799362
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-26 22:57:32 +0700

    Status-enum changed into OrderStatus-enum at every statement in lab3.Order-class

--- src/ru/eltex/app/java/lab3/Order.java
+++ src/ru/eltex/app/java/lab3/Order.java
@@ -1,6 +1,7 @@
 package ru.eltex.app.java.lab3;
 
 import ru.eltex.app.java.lab1.ICrudAction;
+import ru.eltex.app.java.lab2.OrderStatus;
 
 import java.util.*;
 
@@ -16,10 +17,8 @@ public class Order implements ICrudAction {
     protected ShoppingCart cart;
     /** Customer */
     protected Credentials customer;
-    /** Order status enum */
-    protected enum Status {AWAITING, PROCESSED };
     /** Order status enum value */
-    protected Status status;
+    protected OrderStatus status;
 
     public Calendar getCreateTime() {
         return createTime;
@@ -37,7 +36,7 @@ public class Order implements ICrudAction {
 
     /** Default constructor with no params */
     public Order() {
-        status = Status.AWAITING;
+        status = OrderStatus.AWAITING;
         id = UUID.randomUUID();
     }
 
@@ -46,7 +45,7 @@ public class Order implements ICrudAction {
         this.cart = cart;
         this.customer = customer;
         id = UUID.randomUUID();
-        status = Status.AWAITING;
+        status = OrderStatus.AWAITING;
         createTime = new GregorianCalendar();
     }
 
@@ -58,7 +57,7 @@ public class Order implements ICrudAction {
     public void create() {
         //cart.create();
         customer.create();
-        status = Status.AWAITING;
+        status = OrderStatus.AWAITING;
         createTime = new GregorianCalendar();
     }
 
@@ -118,7 +117,7 @@ public class Order implements ICrudAction {
      * @return true if status is PROCESSED
      */
     public boolean isProcessed() {
-        return status == Status.PROCESSED;
+        return status == OrderStatus.PROCESSED;
     }
 
     public ShoppingCart getCart() {
@@ -139,18 +138,22 @@ public class Order implements ICrudAction {
 
     public int setStatus(String status) {
         try {
-            this.status = Status.valueOf(status.toUpperCase());
+            this.status = OrderStatus.valueOf(status.toUpperCase());
         } catch (IllegalArgumentException e) {
-            this.status = Status.AWAITING;
+            this.status = OrderStatus.AWAITING;
             return 1;
         } catch (NullPointerException e) {
-            this.status = Status.AWAITING;
+            this.status = OrderStatus.AWAITING;
             return 2;
         }
         return 0;
     }
 
-    public Status getStatus() {
+    public void setStatus(OrderStatus statement) {
+        status = statement;
+    }
+
+    public OrderStatus getStatus() {
         return status;
     }
 }
