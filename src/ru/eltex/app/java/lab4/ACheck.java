193063c83895854d4024ee6d90d3890717323bcd
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-26 16:35:05 +0700

    Abstract class ACheck developed

---
+++ src/ru/eltex/app/java/lab4/ACheck.java
@@ -0,0 +1,14 @@
+package ru.eltex.app.java.lab4;
+
+import ru.eltex.app.java.lab2.Order;
+
+/**
+ * ACheck class
+ * Check and change orders' status
+ *
+ * @author Dmitry Nevada
+ * @version 0.26.11.19
+ */
+public abstract class ACheck {
+    public abstract int proccess(Order purchase);
+}
