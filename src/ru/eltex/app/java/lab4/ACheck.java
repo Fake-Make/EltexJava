2dfa5fe3b7a2640cded0f0a7f1877df97ced27ae
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-26 22:41:29 +0700

    Java-doc added to ACheck-class

--- src/ru/eltex/app/java/lab4/ACheck.java
+++ src/ru/eltex/app/java/lab4/ACheck.java
@@ -10,5 +10,11 @@ import ru.eltex.app.java.lab2.Order;
  * @version 0.26.11.19
  */
 public abstract class ACheck {
+    /**
+     * Checks orders' status and change it
+     *
+     * @param purchase required order to check status
+     * @return int value that represent result-statement of income purchase
+     */
     public abstract int proccess(Order purchase);
 }
