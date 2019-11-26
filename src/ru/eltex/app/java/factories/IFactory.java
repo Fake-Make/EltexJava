fbb0e408f61885c17540335894c8ff4376177eb2
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-26 19:20:40 +0700

    IFactory-interface developed

---
+++ src/ru/eltex/app/java/factories/IFactory.java
@@ -0,0 +1,11 @@
+package ru.eltex.app.java.factories;
+
+/**
+ * Factory interface
+ *
+ * @author Dmitry Nevada
+ * @version 1.26.11.19
+ */
+public interface IFactory<T> {
+    public T produce();
+}
