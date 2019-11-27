1be646fd1acc6059b7ca338be0d6cae7de9e87a1
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-27 19:44:55 +0700

    Overridden run() method, runTimeout and runTimeoutRange properties moved permanently at ACheck class

--- src/ru/eltex/app/java/lab4/ACheck.java
+++ src/ru/eltex/app/java/lab4/ACheck.java
@@ -3,14 +3,17 @@ package ru.eltex.app.java.lab4;
 import ru.eltex.app.java.lab3.Order;
 import ru.eltex.app.java.lab3.Orders;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * ACheck class
  * Check and change orders' status
  *
  * @author Dmitry Nevada
- * @version 0.26.11.19
+ * @version 1.27.11.19
  */
 public abstract class ACheck extends Thread {
+    protected long runTimeout = 2000, runTimeoutRange = 500;
     protected Orders<Order> ordersList;
 
     public ACheck(Orders<Order> ordersList) {
@@ -22,4 +25,18 @@ public abstract class ACheck extends Thread {
      * @return amount of processed orders
      */
     public abstract int process();
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (ordersList) {
+                process();
+            }
+            try {
+                sleep(ThreadLocalRandom.current().nextLong(runTimeout - runTimeoutRange, runTimeout + runTimeoutRange));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
