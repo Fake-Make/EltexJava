82a60892afbc38715be2b994769efd6f1baf2b1a
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-27 19:17:59 +0700

    Two redundant classes removed

--- src/ru/eltex/app/java/lab4/CheckAwaiting.java
+++
@@ -1,8 +0,0 @@
-package ru.eltex.app.java.lab4;
-
-public class CheckAwaiting extends ACheck {
-    @Override
-    public int process() {
-        return ordersList.processElements();
-    }
-}
--- src/ru/eltex/app/java/lab4/CheckProcessed.java
+++
@@ -1,8 +0,0 @@
-package ru.eltex.app.java.lab4;
-
-public class CheckProcessed extends ACheck {
-    @Override
-    public int process() {
-        return ordersList.removeExpiredElements(false);
-    }
-}
