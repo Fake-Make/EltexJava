0a3edb8c49d1d90546aed04948d61c9e88525b8c
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-29 22:38:33 +0700

    readAll() method developed at lab5.ManagerOrderJson-class

--- src/ru/eltex/app/java/lab5/ManagerOrderJson.java
+++ src/ru/eltex/app/java/lab5/ManagerOrderJson.java
@@ -1,10 +1,15 @@
 package ru.eltex.app.java.lab5;
 
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import ru.eltex.app.java.lab3.Order;
 import ru.eltex.app.java.lab3.Orders;
 
 import java.io.*;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,7 +25,33 @@ public class ManagerOrderJson<T extends Order> extends AManageOrder<T> {
 
     @Override
     public Orders<T> readAll() {
-        return null;
+        Orders<T> readCollection = new Orders<>();
+
+        try (JsonReader inStream = new JsonReader(new FileReader(fileNameToSave))) {
+            Gson gson = new Gson();
+            /** The dark side of Java: reflection **/
+            ArrayList<T> readList = gson.fromJson(inStream, ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+            for (T item : readList) {
+                readCollection.add(item);
+            }
+        } catch (FileNotFoundException eFNF) {
+            System.out.println(eFNF);
+            return null;
+        } catch (IOException eIO) {
+            System.out.println(eIO);
+            return null;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        /**
+         * NOTE: We replace whole existing collection
+         * with collection that we just read.
+         * Probably bad solution, but it depends of what we need.
+         */
+        ordersCollection = readCollection;
+
+        return readCollection;
     }
 
     @Override
