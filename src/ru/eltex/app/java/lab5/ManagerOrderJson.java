fef3fde4c9ff890fb6dadad2af5ce83a0c8a454d
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-29 21:56:21 +0700

    saveAll() method developed at lab5.ManagerOrderJson-class

--- src/ru/eltex/app/java/lab5/ManagerOrderJson.java
+++ src/ru/eltex/app/java/lab5/ManagerOrderJson.java
@@ -1,8 +1,11 @@
 package ru.eltex.app.java.lab5;
 
+import com.google.gson.*;
 import ru.eltex.app.java.lab3.Order;
 import ru.eltex.app.java.lab3.Orders;
 
+import java.io.*;
+import java.util.List;
 import java.util.UUID;
 
 public class ManagerOrderJson<T extends Order> extends AManageOrder<T> {
@@ -22,6 +25,24 @@ public class ManagerOrderJson<T extends Order> extends AManageOrder<T> {
 
     @Override
     public Orders<T> saveAll() {
-        return null;
+        try (FileOutputStream outStream = new FileOutputStream(fileNameToSave)) {
+            List<T> actualOrdersList = ordersCollection.getOrdersList();
+            Gson gson = new Gson();
+            byte[] buffer;
+            for (T item : actualOrdersList) {
+                buffer = ((String) gson.toJson(item)).getBytes();
+                outStream.write(buffer);
+            }
+        } catch (FileNotFoundException eFNF) {
+            System.out.println(eFNF);
+            return null;
+        } catch (IOException eIO) {
+            System.out.println(eIO);
+            return null;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+        return ordersCollection;
     }
 }
