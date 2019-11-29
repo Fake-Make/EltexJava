01592cc324049b27088b2e7f13694e4ecfefb421
Dmitry Nevada <vegnerdima@yandex.ru>
2019-11-29 21:16:48 +0700

    getOrdersList() method added at lab3.Orders-class

--- src/ru/eltex/app/java/lab3/Orders.java
+++ src/ru/eltex/app/java/lab3/Orders.java
@@ -41,6 +41,10 @@ public class Orders<T extends Order> {
         add(order);
     }
 
+    public List<T> getOrdersList() {
+        return ordersList;
+    }
+
     /**
      * Removing item from orders list
      *
